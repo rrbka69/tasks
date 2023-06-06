16421aaa3726f2b425037ea78fb1ac698abb00a2
Наталья Комарова <natkom54@yandex.ru>
2023-06-06 23:28:13 +0700

    второй коммит

---
+++ zadaca1/Program.cs
@@ -0,0 +1,16 @@
+﻿int num,r,sum=0,t;
+Console.Write("Введите пятизначное число: ");
+num = Convert.ToInt32(Console.ReadLine());
+if(num>9999 && num<100000)
+{
+for(t=num;num!=0;num=num/10)
+{
+r=num % 10;
+sum=sum*10+r;
+}
+if(t==sum)
+Console.Write($"{t} является палиндромом.");
+else
+Console.Write($"{t} не является палиндромом.");
+} else
+Console.Write("Введите пятизначное число! ");
